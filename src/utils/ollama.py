867f15d7e1f21743d8ebe70e304a0a49f6dccb7b
qte77 <93844790+qte77@users.noreply.github.com>
2025-01-20 00:58:13 +0000

    test(ollama): server running

--- src/utils/ollama.py
+++ src/utils/ollama.py
@@ -1,4 +1,3 @@
-import ollama
 from requests import get, ConnectionError
 
 
@@ -6,5 +5,14 @@ def check_server_health():
     """Check if the Ollama server is running and responding."""
     try:
         return get("http://127.0.0.1:11434")
+    except ConnectionError as e:
+        return e
+
+
+def get_server_version():
+    """Get the version of the Ollama server."""
+    url = "http://localhost:11434/api/version"
+    try:
+        return get(url).json()
     except ConnectionError as e:
         return e
--- tests/test_ollama.py
+++ tests/test_ollama.py
@@ -2,10 +2,8 @@
 Tests for starting and checking the status of a local Ollama server.
 """
 
-from utils.ollama import check_server_health
-import ollama
+from utils.ollama import check_server_health, get_server_version
 from pytest import fail
-
 
 def test_check_server_health():
     """Test to check if the Ollama server is running and responding."""
@@ -17,4 +15,14 @@ def test_check_server_health():
         ), "Ollama server is not running or not responding."
         print("Ollama server is running and responding correctly.")
     except Exception as e:
+        fail(e)
+
+
+def test_get_server_version():
+    """Test to get the version of the Ollama server."""
+
+    try:
+        response = get_server_version()
+        assert "version" in response, "Response does not contain expected data."
+    except ConnectionError as e:
         fail(e)
